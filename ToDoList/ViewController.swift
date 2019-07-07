2991e88863a5bd78acd4234969a6ec081eecb8fc
Aleksander Shigin <a.sh.10031987@gmail.com>
2019-07-07 21:58:14 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Alexander on 07/07/2019.
+//  Copyright © 2019 Alexander Shigin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
